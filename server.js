b44fdf0e84729ea03dc023927a90e21aa5a22f73
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2026-01-15 16:15:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const https = require('https');
 const GClockBot = require('./bot.js');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const botInstances = new Map();
 const panelSessions = new Map();
